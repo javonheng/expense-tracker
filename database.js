ac5e749415b73617f1028cc67fbdf9b2e305b43f
javon <javonheng@hotmail.com>
2021-05-13 14:54:10 +0800

    proxy

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const db = require('./config');
 
-const url = `mongodb+srv://javonheng:javonheng@smarteventorganizer.okapp.mongodb.net/expense?retryWrites=true&w=majority`;
+const url = db.mongodb;
 
 const connectionParams={
     useNewUrlParser: true,
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const e = require('express');
 const cors = require('cors');
 const db = require('./database');
 const app = express();
